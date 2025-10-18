0877725da9bc0055ab07da96e56323df359f4efc
greenwzrd6 <t4kikun@gmail.com>
2025-10-18 17:09:32 +0200

    added so the script makes a dir and copies the java files into them and moves into it and  writes out the path, also added the dir to .gitignore

--- run_game.sh
+++ run_game.sh
@@ -5,3 +5,25 @@ NAME="David_Lindberg"
 DIR="${NAME}_labb"
 
 echo "${NAME}'s program"
+
+#made an if statement for making the dir and copying the files
+#incase they already exist 
+if [ ! -d "$DIR" ]
+then
+  mkdir "$DIR"
+  echo "Created folder: $DIR"
+else
+  echo "Folder already exists, continuing"
+fi
+
+if [ ! -f "$DIR/GuessingGame.java" ] && [ ! -f "$DIR/Guesser.java" ]
+then
+  cp "se/yrgo/game/GuessingGame.java" "se/yrgo/game/Guesser.java" "$DIR"
+  echo "Copied files to $DIR"
+else
+  echo "Files already exist, continuing"
+fi
+
+cd "$DIR"
+
+echo "Running game from $(pwd)"
