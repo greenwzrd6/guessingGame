2530d0c330a3ad9c9f1bdd22ef9d804da2d217f9
greenwzrd6 <t4kikun@gmail.com>
2025-10-18 17:26:03 +0200

    added so that the script now compiles GuessingGame.java and removes the package declaration at the beginning of both java files

--- run_game.sh
+++ run_game.sh
@@ -16,9 +16,15 @@ else
   echo "Folder already exists, continuing"
 fi
 
+#added so that the package declaration gets removed by not reading the first line
+#then copying the read file into a new file and then changing it name to its original name
 if [ ! -f "$DIR/GuessingGame.java" ] && [ ! -f "$DIR/Guesser.java" ]
 then
   cp "se/yrgo/game/GuessingGame.java" "se/yrgo/game/Guesser.java" "$DIR"
+  for file in "$DIR/GuessingGame.java" "$DIR/Guesser.java"
+  do
+    tail -n +2 "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+  done
   echo "Copied files to $DIR"
 else
   echo "Files already exist, continuing"
@@ -27,3 +33,19 @@ fi
 cd "$DIR"
 
 echo "Running game from $(pwd)"
+
+javac GuessingGame.java
+
+#decided to create an array of dots and then animate a countdown
+#to make the script look a little more fun
+
+dots=("" "." ".." "...")
+
+for d in "${dots[@]}"
+  do
+    echo -ne "Compiling script$d\r"
+    sleep 0.5
+    echo -ne "\r                   \r"
+  done
+
+echo "Done compiling!"
